f3115da7ae06a87ece1033248355fb2976a509fb
Mehran Ali <mehran@booknrent.com>
2025-01-21 12:49:48 +0500

    changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const slackWebhookUrl = "https://hooks.slack.com/services/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";
+const slackWebhookUrl = "https://hooks.slack.com/services/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";
 
 // Middleware to serve static files
 app.use(express.static('public'));
